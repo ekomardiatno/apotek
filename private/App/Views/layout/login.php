cd5c3dc289d212e89b4b080a2994ae876332ca6e
Eko Mardiatno <ekomardiatno@gmail.com>
2023-03-10 23:36:04 +0700

    url brand

--- private/App/Views/layout/login.php
+++ private/App/Views/layout/login.php
@@ -32,7 +32,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark mt-4">
       <div class="container px-4">
-        <a class="navbar-brand" href="<?= Web::url('admin'); ?>">
+        <a class="navbar-brand" href="https://ekomardiatno.github.io">
           <img src="<?= Web::assets('brand/white.png', 'images'); ?>" />
         </a>
       </div>
