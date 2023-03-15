8c766c9c20948be6c8925958fccfe8ebb0003fb2
Eko Mardiatno <ekomardiatno@gmail.com>
2023-03-15 23:43:31 +0700

    add controllers on init

--- private/App/init.php
+++ private/App/init.php
@@ -1,14 +1,12 @@
 <?php
 
-$dir = ['/Core/', '/Wrapper/', '/Helpers/'];
-foreach($dir as $d) {
+$dir = ['/Core/', '/Wrapper/', '/Helpers/', '/Controllers/'];
+foreach ($dir as $d) {
     spl_autoload_register(function ($class) use ($d) {
-    
+
         if (file_exists(__DIR__ . $d . $class . '.php')) {
-    
+
             require_once __DIR__ . $d . $class . '.php';
-            
         }
-    
     });
 }
